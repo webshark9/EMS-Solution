b9178d04fe11655e45df05f3eda4df813df0628b
wboldt-cc <wboldt-cc@conestogac.on.ca>
2014-11-26 22:04:28 -0500

    Added more code to the Logging Class

--- Supporting/Logging.cs
+++ Supporting/Logging.cs
@@ -21,8 +21,8 @@ namespace Supporting
     /// \authors Matthew Thiessen, Willi Boldt, Ping Chang Ueng, and Tylor McLaughlin
     class Logging
     {
-        private static StreamWriter logFileWriter;///< the stream used to write to the log file
-        private static DateTime lastDate;///< the last day that something was logged (used to tell if a new log file needs to be created)
+        private static StreamWriter logFileWriter = null;///< the stream used to write to the log file
+        private static DateTime lastDate = new DateTime();///< the last day that something was logged (used to tell if a new log file needs to be created)
 
         /**
         * \brief To create/open (for appending) the log file for the day
@@ -40,7 +40,9 @@ namespace Supporting
             string fileName = "";
             lastDate = DateTime.Today;
 
-            fileName += lastDate.ToString() + "_EMS_Log_File";
+            //fileName += lastDate.ToString() + "_EMS_Log_File";
+            fileName += lastDate.Year.ToString() + "-" + lastDate.Month.ToString() + "-" + lastDate.Day.ToString() + "_EMS_Log_File";
+
 
             logFileWriter = new StreamWriter(fileName, true);
         }
@@ -50,7 +52,7 @@ namespace Supporting
         * \brief To close the log file for the day
         * \details <b>Details</b>
         *
-        * This method does nothing special
+        * This method simply sets the <i>logFileWriter</i> data member to null.
         * 
         * \param None
         * 
@@ -59,7 +61,7 @@ namespace Supporting
         */
         public static void CloseLogFile()
         {
-
+            logFileWriter = null;
         }
 
 
@@ -81,17 +83,30 @@ namespace Supporting
         public static bool LogEvent(string eventString)
         {
             bool logSuccessful = false;
+            string logString = "";// the string that gets written to the log file
 
-            if (lastDate != DateTime.Today)// check if we are going to write the same log file as our last write or if we need to create a new log file
+            if ((lastDate == null) || (lastDate != DateTime.Today))// check if we are going to write the same log file as our last write or if we need to create a new log file
             {
                 // need to open a new log file
                 CloseLogFile();
                 OpenLogFile();
                 
-
             }
 
-            logFileWriter.WriteLine("test " + eventString);
+            logString = lastDate.Year.ToString() + "-" + lastDate.Month.ToString() + "-" + lastDate.Day.ToString();// add the YYYY-MM-DD
+            logString += " " + lastDate.Hour.ToString() + ":" + lastDate.Minute.ToString() + ":" + lastDate.Second.ToString();// add the HH:MM:SS
+            logString += " " + eventString;
+
+            try
+            {
+
+                logFileWriter.WriteLine(logString);
+                logSuccessful = true;
+            }
+            catch(Exception e)
+            {
+                // display in console
+            }
 
             return logSuccessful;
         }
